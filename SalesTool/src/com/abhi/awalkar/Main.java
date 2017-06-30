4235976a648995e3989a7798649d3968a94ed564
abhishekawalkar <abhishek.awalkar@gmail.com>
2017-07-01 00:27:05 +0530

    Initial Checking of sales tool.
    Change-Id: I942e2f8c22d6866a18c1f608330d8f597c5c0316

---
+++ SalesTool/src/com/abhi/awalkar/Main.java
@@ -0,0 +1,21 @@
+package com.abhi.awalkar;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data=new SalesData();
+		
+		displayGreeting();
+		data.display();
+		
+
+	}
+	
+	private static void displayGreeting()
+	{
+		System.out.println("Hello Happy Sales People!");
+		System.out.println("This app shows sales data");
+	}
+
+}
