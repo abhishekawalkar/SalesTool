5b89b1c52b4dd2c799125e2a2ada30332bd227eb
abhishekawalkar <abhishek.awalkar@gmail.com>
2017-07-01 01:19:48 +0530

    rahil ki

---
+++ SalesTool1/src/com/abhi/awalkar/Main.java
@@ -0,0 +1,22 @@
+package com.abhi.awalkar;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Test t=new Test();
+		t.test();
+		test2();
+	}	
+		 public static void test2()
+		{
+			System.out.println("this is test2");
+			System.out.println("kay kay");
+			System.out.println("jammahuda");
+		}
+
+	}
+
+
